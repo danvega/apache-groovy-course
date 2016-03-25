789d9520dc935bf768ab5da341560e94b16627f0
Dan Vega <danvega@gmail.com>
2016-03-25 16:48:04 -0400

    range demos

---
+++ collections/ranges/ForLoops.groovy
@@ -0,0 +1,16 @@
+for( int x = 1; x <= 10; ++x) { 
+    print x
+}
+
+println ""
+
+for( int y = 10; y >= 1; --y) {
+    print y
+}
+
+println ""
+
+def letters = ['a','b','c']
+for( int i = 0; i < letters.size(); ++i) {
+    print letters[i]
+}
