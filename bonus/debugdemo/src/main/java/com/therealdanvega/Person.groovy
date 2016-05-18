3f532570512ca3f5b5f659678055d48ab22599d7
Dan Vega <danvega@gmail.com>
2016-05-18 11:45:49 -0400

    debug demo updates

---
+++ bonus/debugdemo/src/main/java/com/therealdanvega/Person.groovy
@@ -0,0 +1,13 @@
+package com.therealdanvega
+
+import groovy.transform.ToString
+
+@ToString
+class Person {
+
+    String first
+    String last
+    String email
+    Date dob
+
+}
---
+++ bonus/debugdemo/src/main/java/com/therealdanvega/app.groovy
@@ -0,0 +1,6 @@
+package com.therealdanvega
+
+Person p = new Person(first:"Dan",last: "Vega",email:"danvega@gmail.com",dob: new Date() )
+
+println p.toString()
+
