05f8c90c3163489c20e4eaf5bdf3ce99e41535ab
Dan Vega <danvega@gmail.com>
2016-05-18 13:25:15 -0400

    bonus demos

---
+++ bonus/HelloGroovy/src/main/groovy/com/therealdanvega/HelloGroovyApplication.groovy
@@ -0,0 +1,12 @@
+package com.therealdanvega
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HelloGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run HelloGroovyApplication, args
+	}
+}
