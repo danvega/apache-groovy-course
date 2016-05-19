792e813b373f5703176f3742ceb4ae33154073d3
Dan Vega <danvega@gmail.com>
2016-05-18 20:06:44 -0400

    ast transformations additions

--- ast/transformations/src/typechecked/Person.groovy
+++ ast/transformations/src/typechecked/Person.groovy
@@ -1,13 +1,14 @@
 package typechecked
 
-import groovy.transform.Canonical
 import groovy.transform.TypeChecked
 
-@Canonical
 @TypeChecked
 class Person {
 
-    String first
-    String last
+    String firstName
+    String lastName
 
+    String getFullName(){
+        "$firstName $lastName"
+    }
 }
